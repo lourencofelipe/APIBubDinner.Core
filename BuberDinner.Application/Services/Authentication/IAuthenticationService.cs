a6ff95d4c8352e9acadec859da4fb8817ce3a0e7
lourencofelipe <felipe.pucsc@gmail.com>
2022-07-21 21:11:54 -0300

    feat: Adding Authentication Services

---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,8 @@
+namespace BuberDinner.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthenticationResult Login(string firstName, string lastName, string email, string password);
+
+    AuthenticationResult Register(string email, string password);
+}
