4a3220523f90a9c20539851031819cb4cff55489
lourencofelipe <felipe.pucsc@gmail.com>
2022-07-25 08:32:19 -0300

    fix: changing service methods

--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -3,24 +3,26 @@ namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
-    public AuthenticationResult Login(string firstName, string lastName, string email, string password) 
+    public AuthenticationResult Register(string firstName, string lastName, string email, string password) 
     {
         return new AuthenticationResult(
             Guid.NewGuid(), 
-            "firstName", 
-            "lastName", 
-            email, 
-            password);
-    }
-
-    public AuthenticationResult Register(string email, string password)
-    { 
-         return new AuthenticationResult(
-            user.Id, 
             firstName, 
             lastName, 
             email, 
-            password);
+            password,
+            "token");
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    { 
+         return new AuthenticationResult(
+            Guid.NewGuid(), 
+            "firstName", 
+            "lastName", 
+            email,
+            password,
+            "token");
     }
 
 }
