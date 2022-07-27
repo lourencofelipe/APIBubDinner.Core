f442d8c3eae700379e0ae32ad786ae23096f2bf8
lourencofelipe <felipe.pucsc@gmail.com>
2022-07-27 17:03:17 -0300

    feat: Adding Token generator

--- BuberDinner.Infrastructure/DependencyInjection.cs
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,9 @@
+using BuberDinner.Application.Common.Interfaces.Authentication;
+using BuberDinner.Infrastructure.Authentication;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using BuberDinner.Infrastructure;
+
 
 namespace BuberDinner.Infrastructure;
 
@@ -7,11 +11,12 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, ConfigurationManager configuration)
     {
-        // services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        //services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
         // services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
         // services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
 
-      return services;
+        return services;
     }
 }
 
